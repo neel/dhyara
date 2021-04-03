8c1d9b995f97761768b5ab5ed0822156746f8f3e
Neel Basu <neel.basu.z@gmail.com>
2021-04-03 18:21:11 +0530

    - ping showing duration and bytes

--- includes/dhyara/tools/ping.h
+++ includes/dhyara/tools/ping.h
@@ -18,6 +18,17 @@ namespace tools{
 /**
  * ping tool sends ICMP requests in batches. Each batch contains <tt>batch</tt> number of requests. Number of batches is <tt>count</tt>. 
  * No explicit delay between two requests in the same batch. Sleep between two consecutive batches in <tt>sleep</tt> ms.
+ * It reports the min, max, avg and total RTT (Round Trip Time) between the source and the target.
+ * It also reports the total duration as half of the difference between the first sent time and last received time.
+ * Following is an example of ping output. The target node's mac address is 4c:11:ae:9c:a6:85. tx/rx are the packets or bytes transmitted vs received.
+ * In this example it took ms 12.7 for the last ICMPR to be received since the first ICMPQ was sent. 
+ * According to this report 6.35 ms is the expected time, it may take for the target node to receive a 5000 byte long payload transmitted by the source node, in the current condition of wireless medium.
+ * \code
+ * I (41130) ping: 4c:11:ae:9c:a6:85
+ * I (41140) ping: waiting 1s for all replies
+ * I (42140) ping: 20/20 received, 0.00% lost, rtt min/avg/max/total = 1.07/1.24/1.78/24.85 ms
+ * I (42140) ping: 5000/5000 bytes in 12.7 ms
+ * \endcode
  * \ingroup tools
  */
 struct ping{
@@ -72,12 +83,28 @@ struct ping{
      * \param addr target address to ping
      */
     void operator()(const std::string& addr);
+
+    /**
+     * Reset before starting another traceroute
+     */
+    void reset();
+    
+    ~ping();
+    
+    private:
+        /**
+         * The callback to capture the reply packet and print the destination till which the previous ICMP request packet has reached. If that destination is the desired destination then stop sending another ICMP request. Otherwise send another with increased ttl value.
+         */
+        void reply(const dhyara::peer_address& addr, const dhyara::packets::echo_reply& reply);
     
     private:
         dhyara::network& _network;
         std::uint8_t     _count;
         std::int8_t      _batch;
         std::uint8_t     _sleep;
+        std::size_t      _conn_reply;
+        delay_type       _first;
+        delay_type       _last;
 };
     
 }
--- sources/tools/ping.cpp
+++ sources/tools/ping.cpp
@@ -26,16 +26,18 @@
  */
 
 #include "dhyara/tools/ping.h"
+#include <inttypes.h>
 
-dhyara::tools::ping::ping(dhyara::network& network, std::uint8_t count, std::int8_t batch, std::uint8_t sleep): _network(network), _count(count), _batch(batch), _sleep(sleep){}
+dhyara::tools::ping::ping(dhyara::network& network, std::uint8_t count, std::int8_t batch, std::uint8_t sleep): _network(network), _count(count), _batch(batch), _sleep(sleep), _first(0), _last(0){
+    using namespace std::placeholders;
+    _conn_reply = _network.echo_reply().connect(std::bind(&dhyara::tools::ping::reply, this, _1, _2));
+}
 
 void dhyara::tools::ping::operator()(const dhyara::peer_address& addr){
     ESP_LOGI("ping", "%s", addr.to_string().c_str());
     
-    _network.link().reset(dhyara::packets::type::echo_request);
-    _network.link().reset(dhyara::packets::type::echo_reply);
-    _network.echo_reply().reset();
-    
+    reset();
+    _first = esp_timer_get_time();
     _network.echo_request().ping(addr, _count, _batch, _sleep);
     ESP_LOGI("ping", "waiting 1s for all replies");
     vTaskDelay(pdMS_TO_TICKS(1000));
@@ -43,6 +45,7 @@ void dhyara::tools::ping::operator()(const dhyara::peer_address& addr){
     std::size_t icmp_sent  = _network.link().tx(dhyara::packets::type::echo_request);
     std::size_t icmp_rcvd  = _network.link().rx(dhyara::packets::type::echo_reply);
     double icmp_loss  = (double)(icmp_sent - std::min(icmp_rcvd, icmp_sent)) / (double)icmp_sent;
+    dhyara::delay_type wastage = ((_count-1)*_sleep)*1000;
     
     ESP_LOGI(
         "ping", 
@@ -55,9 +58,35 @@ void dhyara::tools::ping::operator()(const dhyara::peer_address& addr){
         (double)_network.echo_reply().rtt().max/1000.0,
         (double)_network.echo_reply().rtt().total/1000.0
     );
+    ESP_LOGI(
+        "ping", 
+        "%zu/%zu bytes in %.2lf ms", 
+        icmp_rcvd*250,
+        icmp_sent*250,
+        (double)(_last - _first - wastage)/1000.0
+    );
+    reset();
 }
 
 void dhyara::tools::ping::operator()(const std::string& addr){
     operator()(dhyara::peer_address(addr));
+}
+
+void dhyara::tools::ping::reply(const dhyara::peer_address&, const dhyara::packets::echo_reply& reply){
+    _last = esp_timer_get_time();
+}
+
+void dhyara::tools::ping::reset(){
+    _network.link().reset(dhyara::packets::type::echo_request);
+    _network.link().reset(dhyara::packets::type::echo_reply);
+    _network.echo_reply().reset();
+    
+    _last  = 0;
+    _first = 0;
+}
+
+dhyara::tools::ping::~ping(){
+    _network.echo_reply().disconnect(_conn_reply);
+    reset();
 }
 
