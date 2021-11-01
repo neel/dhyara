0ba385438c8e2609a09d11c83f24226cd8381e0a
Neel Basu <neel.basu.z@gmail.com>
2021-11-01 16:00:07 +0530

    - hello-cxx example rolledback

--- examples/hello-cxx/main/mainx.cpp
+++ examples/hello-cxx/main/mainx.cpp
@@ -15,7 +15,7 @@ void mainx(){
     dhyara::network network(dhyara_link());
     dhyara_set_default_network(&network);
     
-    // dhyara::address sink("ac:67:b2:25:8e:a5"), source("4c:11:ae:9c:1a:c9");
+    dhyara::address sink("ac:67:b2:25:8e:a5"), source("4c:11:ae:9c:1a:c9");
     
     network.start();
     
@@ -26,30 +26,30 @@ void mainx(){
      */
     // network.isolate(source, sink);
     
-    // dhyara::address local = dhyara_local();
-    // dhyara::address other = (local == source) ? sink : ((local == sink) ? source : dhyara::address::null());
+    dhyara::address local = dhyara_local();
+    dhyara::address other = (local == source) ? sink : ((local == sink) ? source : dhyara::address::null());
     
     /**
      * The anonymous function will be called once all chunks of a data packet is received
      */
-    // network.on_data([](const dhyara::address& source, const dhyara::packets::data& data){
-    //     std::cout << "received data " << " originating from " << data.source() << " via " << source << " of size " << data.length() << std::endl;
-    // });
+    network.on_data([](const dhyara::address& source, const dhyara::packets::data& data){
+        std::cout << "received data " << " originating from " << data.source() << " via " << source << " of size " << data.length() << std::endl;
+    });
     
     while(1){
-        // if(!other.is_null()){
-        //     dhyara::tools::ping ping(network);
-        //     ping.count(1).batch(50).sleep(0);
-        //     ping(other);
-        //     vTaskDelay(pdMS_TO_TICKS(2000));
+        if(!other.is_null()){
+            dhyara::tools::ping ping(network);
+            ping.count(1).batch(50).sleep(0);
+            ping(other);
+            vTaskDelay(pdMS_TO_TICKS(2000));
 
-        //     dhyara::tools::traceroute traceroute(network, other);
-        //     traceroute();
-        //     vTaskDelay(pdMS_TO_TICKS(2000));
+            dhyara::tools::traceroute traceroute(network, other);
+            traceroute();
+            vTaskDelay(pdMS_TO_TICKS(2000));
 
-        //     std::string buffer = "Hello World";
-        //     network.send(other, buffer.begin(), buffer.end());
-        // }
+            std::string buffer = "Hello World";
+            network.send(other, buffer.begin(), buffer.end());
+        }
         vTaskDelay(pdMS_TO_TICKS(5000));
     }
 } 
